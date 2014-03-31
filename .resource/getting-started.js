ad9549a0dcbd113586668c4c715507de5adf15c8
alu <alu.ula@outlook.com>
2014-03-31 14:39:22 +0900

    ２段階認証向けにgetting statedを調整

--- .resource/getting-started.js
+++ .resource/getting-started.js
@@ -2,11 +2,15 @@ $(function() {
     ko.applyBindings({
         hostname: location.hostname,
         user: ko.observable(null),
-        repository: ko.observable(null)
+        repository: ko.observable(null),
+        authToken: ko.observable(null),
+        twoStepAuth: ko.observable(false)
     });
+
     $('input').keypress(function(e) {
         if (e.keyCode === 13) {
             $(this).blur().parent().next().find('input').focus();
         }
-    })
+    });
+
 });
